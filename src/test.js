b5d6354aacc343fc680d4507ae9e17c52198288f
Andrey Petrikov <aapetrikov@gmail.com>
2020-07-04 15:39:52 +0300

    init

---
+++ src/test.js
@@ -0,0 +1,26 @@
+
+import Character from "../src/index.js";
+
+test("new Character", () => {
+    const result = new Character("Тор","Daemon");
+    const expected =
+        {"Bowman": "25/25", "Daemon": "10/40", "Magician": "10/40", "Swordsman": "40/10", "Undead": "25/25", "Zombie": "40/10", "health": 100, "level": 1, "name": "Тор", "type": "Daemon"};
+    expect(result).toEqual(expected);
+})
+test("error name Character", () => {
+    const result = new Character("r","Daemon");
+    const expected = "Имя должно быть от 2 до 10 символов";
+    expect(result).toThrow(expected);
+})
+test("error type Character", () => {
+    const result = new Character("Tor","Мечник");
+    const expected = "Тип персонажа не найден";
+    expect(result).toThrow(expected);
+})
+test("damage ", () => {
+    const person = new Character("Тор","Daemon");
+    person.damage(99);
+    const result = person.health;
+    const expected = 40.6;
+    expect(result).toBe(expected);
+})
