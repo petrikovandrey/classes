5b62366aa9eb1fe026429c4b19cc45053ba12e08
petrikovandrey <aapetrikov@gmail.com>
2021-06-14 16:53:07 +0300

    fix tests

--- src/test.js
+++ src/test.js
@@ -17,24 +17,24 @@ test("new Bowerman", () => {
 test("levelUp Bowerman health = 0 ", () => {
     const person = new index.Bowerman("Gena", "Bowerman");
     person.health = 0;
-    const result = person.levelUp();
-    const expected = "нельзя повысить левел умершего";
-    expect(result).toBe(expected);
+    expect(() => { person.levelUp() }).toThrow();
 })
 test("level up Bowerman", () => {
     const person = new index.Bowerman("Gena", "Bowerman");
     person.levelUp();
     const expected =
-        { "health": 100, "level": 2, "name": "Gena", "attack": 30, "defence": 30 ,"type": "Bowerman"};
+        { "health": 100, "level": 2, "name": "Gena", "attack": 30, "defence": 30, "type": "Bowerman" };
     expect(person).toEqual(expected);
 })
 test("error name Character", () => {
-    const result = new index.Character("G", "Daemon");
-    expect(result).toThrow();
+    expect(() => {
+        new index.Character("G", "Daemon");
+    }).toThrow();
 })
 test("error type Character", () => {
-    const result = new index.Character("Gena", "Мечник");
-     expect(result).toThrow();
+    expect(() => {
+        new index.Character("Gena", "Мечник")
+    }).toThrow();
 })
 test("damage point", () => {
     const person = new index.Daemon("Тор", "Daemon");
